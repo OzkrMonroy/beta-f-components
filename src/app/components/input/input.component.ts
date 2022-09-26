4227313235e25e3dc66f80c46de10d9e294781d6
OscarMonroy <osmonroy@bi.com.gt>
2022-09-26 17:19:48 -0600

    Input element now receives more info by input angular option.

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { InputColors } from 'src/app/types/input';
+import { InputAutoComplete, InputColors, InputType } from 'src/app/types/input';
 
 @Component({
   selector: 'app-input',
@@ -9,15 +9,21 @@ import { InputColors } from 'src/app/types/input';
 })
 export class InputComponent {
   form: FormGroup = this.formBuilder.group({
-    name: ['', Validators.required],
+    ['default-name']: ['', Validators.required],
   });
   
   @Input() color: InputColors = 'primary';
   @Input() containerClasses: string = ''
+  @Input() id: string = 'default-id'
+  @Input() name: string = 'default-name'
+  @Input() autoComplete: InputAutoComplete = 'off'
+  @Input() type: InputType = 'text'
+  @Input() lable: string = 'Default label'
+  @Input() required: boolean = false;
 
   constructor(private readonly formBuilder: FormBuilder) {}
 
   mustFloat() {
-    return this.form.get('name')?.value !== '';
+    return this.form.get(this.name)?.value !== '';
   }
 }
--- src/app/types/input.ts
+++ src/app/types/input.ts
@@ -1,1 +1,3 @@
-export type InputColors = 'primary' | 'error' | 'success'
+export type InputColors = 'primary' | 'error' | 'success';
+export type InputAutoComplete = 'on' | 'off';
+export type InputType = 'text' | 'email' | 'number' | 'password' | 'submit' | 'tel' | 'decimal'
